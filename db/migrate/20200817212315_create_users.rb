ddbd93554025f4bb4fd91015ab1a5e7fe875f9f1
Jack Fragassi <jfragassi98@gmail.com>
2020-08-17 14:38:14 -0700

    Build user table/model

---
+++ db/migrate/20200817212315_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.index :username, unique: true
+      t.index :session_token, unique: true
+    end
+  end
+end
